1a245b1fd15e177014ef82dec23f509fb1aba38c
pgcity <tkym1394@gmail.com>
2018-12-31 14:36:18 +0900

    UIを作成

---
+++ QuickEvidence/QuickEvidence/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickEvidence.ViewModels
+{
+	public class MainWindowViewModel : BindableBase
+	{
+        public MainWindowViewModel()
+        {
+
+        }
+
+        private string _folderPath = "FolderPath";
+        public string FolderPath
+        {
+            get { return _folderPath; }
+            set { SetProperty(ref _folderPath, value); }
+        }
+    }
+}
