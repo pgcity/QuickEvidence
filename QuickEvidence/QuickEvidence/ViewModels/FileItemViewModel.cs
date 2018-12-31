5ab6fa5b8141c6ab6abc0900ac2c97bd8ec4da36
pgcity <tkym1394@gmail.com>
2018-12-31 19:55:03 +0900

    ファイル一覧の表示

---
+++ QuickEvidence/QuickEvidence/ViewModels/FileItemViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickEvidence.ViewModels
+{
+	public class FileItemViewModel : BindableBase
+	{
+        public FileItemViewModel()
+        {
+
+        }
+
+        /// <summary>
+        /// ファイル名
+        /// </summary>
+        private string _fileName;
+        public string FileName
+        {
+            get { return _fileName; }
+            set { SetProperty(ref _fileName, value); }
+        }
+
+        /// <summary>
+        /// フォルダーパス
+        /// </summary>
+        private string _folderPath;
+        public string FolderPath
+        {
+            get { return _folderPath; }
+            set { SetProperty(ref _folderPath, value); }
+        }
+
+        /// <summary>
+        /// フォルダーフルパス
+        /// </summary>
+        private string _folderFullPath;
+        public string FolderFullPath
+        {
+            get { return _folderFullPath; }
+            set { SetProperty(ref _folderFullPath, value); }
+        }
+    }
+}
--- QuickEvidence/QuickEvidence/ViewModels/MainWindowViewModel.cs
+++ QuickEvidence/QuickEvidence/ViewModels/MainWindowViewModel.cs
@@ -3,8 +3,10 @@ using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Windows;
 
 namespace QuickEvidence.ViewModels
 {
@@ -18,8 +20,26 @@ namespace QuickEvidence.ViewModels
                 FolderPath = Directory.GetCurrentDirectory();
                 Properties.Settings.Default.Save();
             }
+
+            UpdateFileList();
         }
 
+        ///////////////////////////////////////////////
+        // プロパティ
+
+        /// <summary>
+        /// ファイル一覧
+        /// </summary>
+        private ObservableCollection<FileItemViewModel> _fileItems = new ObservableCollection<FileItemViewModel>();
+        public ObservableCollection<FileItemViewModel> FileItems
+        {
+            get { return _fileItems; }
+            set { SetProperty(ref _fileItems, value); }
+        }
+
+        /// <summary>
+        /// フォルダパス
+        /// </summary>
         private string _folderPath = "FolderPath";
         public string FolderPath
         {
@@ -27,6 +47,13 @@ namespace QuickEvidence.ViewModels
             set { SetProperty(ref _folderPath, value); }
         }
 
+
+        ///////////////////////////////////////////////
+        // コマンド
+
+        /// <summary>
+        /// フォルダー選択ダイアログ
+        /// </summary>
         private DelegateCommand _folderSelectCommand;
         public DelegateCommand FolderSelectCommand =>
             _folderSelectCommand ?? (_folderSelectCommand = new DelegateCommand(ExecuteFolderSelectCommand));
@@ -35,15 +62,21 @@ namespace QuickEvidence.ViewModels
         {
             var dlg = new CommonOpenFileDialog("フォルダー選択");
             dlg.IsFolderPicker = true;
+            dlg.EnsurePathExists = true;
             var ret = dlg.ShowDialog();
             if (ret == CommonFileDialogResult.Ok)
             {
                 FolderPath = dlg.FileName;
                 Properties.Settings.Default.FolderPath = FolderPath;
                 Properties.Settings.Default.Save();
+
+                UpdateFileList();
             }
         }
 
+        /// <summary>
+        /// エクスプローラーで開く
+        /// </summary>
         private DelegateCommand _openExplorerCommand;
         public DelegateCommand OpenExplorerCommand =>
             _openExplorerCommand ?? (_openExplorerCommand = new DelegateCommand(ExecuteOpenExplorerCommand));
@@ -53,6 +86,50 @@ namespace QuickEvidence.ViewModels
             if (Directory.Exists(FolderPath))
             {
                 System.Diagnostics.Process.Start(FolderPath);
+            }
+        }
+
+        private DelegateCommand _updateFileListCommand;
+        public DelegateCommand UpdateFileListCommand =>
+            _updateFileListCommand ?? (_updateFileListCommand = new DelegateCommand(ExecuteUpdateFileListCommand));
+
+        void ExecuteUpdateFileListCommand()
+        {
+            UpdateFileList();
+        }
+
+        ///////////////////////////////////////////////
+        // ロジック
+
+        /// <summary>
+        /// ファイル一覧の更新
+        /// </summary>
+        void UpdateFileList()
+        {
+            FileItems.Clear();
+
+            if (!Directory.Exists(FolderPath))
+            {
+                return;
+            }
+
+            IEnumerable<string> list = new List<string>();
+            try
+            {
+                list = Directory.GetFiles(FolderPath, "*.*", SearchOption.AllDirectories);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            foreach (var item in list)
+            {
+                FileItems.Add(new FileItemViewModel()
+                {
+                    FileName = Path.GetFileName(item),
+                    FolderPath = Path.GetDirectoryName(item).Replace(FolderPath, "."),
+                    FolderFullPath = Path.GetDirectoryName(item)
+                });
             }
         }
     }
