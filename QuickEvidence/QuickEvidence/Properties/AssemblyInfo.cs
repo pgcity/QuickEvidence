c14d88b2b56ed75193f067f2dc49589c44d6d6b3
pgcity <tkym1394@hotmail.com>
2019-01-02 20:45:27 +0900

    readme.txt(メモ)作成

--- QuickEvidence/QuickEvidence/Properties/AssemblyInfo.cs
+++ QuickEvidence/QuickEvidence/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QuickEvidence")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019 Hiroaki Takeyama")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
