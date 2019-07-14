1f8b37775312f210bd6f2af4d5fd26b57224da2b
pgcity <tkym1394@hotmail.com>
2019-07-14 17:59:35 +0900

    #15 タイトルバーに開いているファイル名を表示する

---
+++ QuickEvidence/QuickEvidence/Views/WindowTitleConverter.cs
@@ -0,0 +1,28 @@
+﻿using QuickEvidence.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace QuickEvidence.Views
+{
+    /// <summary>
+    /// 選択ファイル名からWindowタイトルに変換する
+    /// </summary>
+    public class WindowTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null)
+            {
+                return parameter;
+            }
+            var fileItem = value as FileItemViewModel;
+            return fileItem.FileName + " - " + parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
