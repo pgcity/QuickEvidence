440634fab5f381c0c448f4413d100a49db6d0e25
pgcity <tkym1394@hotmail.com>
2020-01-14 22:09:20 +0900

    バージョン更新

--- QuickEvidence/QuickEvidence/Properties/AssemblyInfo.cs
+++ QuickEvidence/QuickEvidence/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QuickEvidence")]
-[assembly: AssemblyCopyright("Copyright ©  2019 Hiroaki Takeyama")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Hiroaki Takeyama")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.1")]
-[assembly: AssemblyFileVersion("1.4.0.1")]
+[assembly: AssemblyVersion("1.5.0.1")]
+[assembly: AssemblyFileVersion("1.5.0.1")]
